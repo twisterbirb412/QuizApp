c826db5510b0de4906781638e349a5fbfea86b60
Candace Joy Metsker <candacejmetsker@gmail.com>
2019-07-12 21:11:38 -0400

    7-12 21:11 JS Pseudocode Complete

--- index.js
+++ index.js
@@ -8,16 +8,17 @@
 //The starting screen 
 //should have a button that users can click to start the quiz.
 
-//button event listeners
+//-----button event listeners----------------
 function startButtonPress() {
     //event listener for start button
 }
 
 function restartButtonPress() {
-
+    //listen for when restart button press
+    startQuiz();
 }
 
-function submitQuestion() {
+function buttonSubmitQuestion() {
     //happens when user hits Submit on question
     //user must select an answer before triggering next events
     //needs to trigger...
@@ -27,17 +28,18 @@ function submitQuestion() {
         showResults();
 }
 
-//initializing quiz
-function startQuiz() {
-    //restarts the quiz at question 1
-    //changes second class of 'bookend-screen' to 'quiz-container'
-    resetCountAndScore();
-    createImageContainer();
-    createQuizContainer();
-    createQuestionResults();
-    createQuizForm();
-    nextQuestion();
-    nextImage();
+function buttonContinueQuiz() {
+    //listens for when user presses continue button
+    continueQuiz();
+    //need to assess whether it is the last question of the quiz
+}
+
+//-----initializing quiz-----------------------
+
+function clearMainContainer() {
+    //clears all contents within main container with the
+    //exception of the button
+
 }
 
 function resetCountAndScore() {
@@ -61,6 +63,23 @@ function createQuizForm() {
     //adds html text to quiz form to show quiz
 }
 
+function clearVariables(){
+    //resets question count and score variables
+}
+
+function startQuiz() {
+    //restarts the quiz at question 1
+    //changes second class of 'bookend-screen' to 'quiz-container'
+    resetCountAndScore();
+    createImageContainer();
+    createQuizContainer();
+    createQuestionResults();
+    createQuizForm();
+    clearVariables();
+    nextQuestion();
+    nextImage();
+}
+
 //----action functions----------------
 
 function clearQuizForm() {
@@ -70,8 +89,10 @@ function clearQuizForm() {
 
 function nextQuestion() {
     clearQuizForm();
+    
     //pulls array data to next question
     //adds html code to quiz form to create answer data
+    //puts question count variable count+1
 }
 
 function clearImageContainer() {
@@ -83,12 +104,23 @@ function nextImage() {
     //pulls image from array and puts it in image container
 }
 
+function clearQuestionResults(){
+    //clears out text between <p> to just a space
+}
+
+function continueQuiz() {
+    nextQuestion();
+    nextImage();
+    clearQuestionResults();
+}
 
 //------after answer is submitted-------------
 
 function showResults() {
     toggleLabelClasses();
     toggleSubmittedAnswer();
+    assessAnswer();
+    submitButtonToContinue();
 }
 
 function toggleLabelClasses() {
@@ -99,5 +131,36 @@ function toggleLabelClasses() {
 function toggleSubmittedAnswer() {
     //calls variable that stores the user's answer
     //adds class 'selected' to the label class
+}
 
+function assessAnswer() {
+    //compares whether use's submitted value is correct
+    //if statement - if the user's answer is the same as the
+    //value that has the value correct
+        //add text to the question-results as "Your answer is correct"
+    //else
+        //add text to the question-results as "Your answer is incorrect!  T
+        //...The correct answer is __"
+}
+
+function submitButtonToContinue(){
+    //changes the label of the function-button to Continue
+    //instead of Submit Answer label changes to Continue
+}
+
+//-----End of Quiz---------
+
+function toggleMainContainerToBookend() {
+    //change main-container class from quiz-container to bookend-container
+}
+
+function addEndText() {
+    //add code to main-container bookend-screen
+    //<h1>How well do you know your birds?</h1>
+    //<h2>You identified VARIABLE out of 10 birds correctly!</h2>
+}
+
+function endOfQuiz() {
+    clearMainContainerToBookend();
+    addEndText();
 }
