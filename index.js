630809e2d76f9dde93116c8ed5b7a464c7004c24
Candace Joy Metsker <candacejmetsker@gmail.com>
2019-07-18 19:42:49 -0400

    July 18 19:42

--- index.js
+++ index.js
@@ -1,11 +1,107 @@
 `use strict`;
 
+//Can you name the bird?
+
+QUIZDATA = [
+
+    //Question1
+    {
+    image: `https://images.wildaboutbirds.com/backyard-birds/bluejay.jpg`,
+    answer1: `Cardinal`,
+    answer2: `Bluejay__correct`,
+    answer3: `Kingfisher`,
+    answer4: `White-breasted nuthatch`
+    },
+    //Question2
+    {
+    image: `https://live.staticflickr.com/2911/32250438734_05bb5959cd.jpg`,
+    answer1: `White-Breasted Nuthatch__correct`,
+    answer2: `Tufted Titmouse`,
+    answer3: `Black capped chickadee`,
+    answer4: `Mockingbird`
+    },
+    
+    //Question3
+    {
+    image: `https://live.staticflickr.com/4706/38902175905_182fdf1560.jpg`,
+    answer1: `Pine thrush`,
+    answer2: `Black-capped chickadee`,
+    answer3: `Carolina chickadee__correct`,
+    answer4: `English Sparrow`
+    },
+    
+    //Question4
+    {
+    image: `http://jaysbirdbarn.com/wp-content/uploads/2016/11/jamesmorrismourningdove.jpg`,
+    answer1: `Mockingbird`,
+    answer2: `Mourning dove__correct`,
+    answer3: `Tufted titmouse`,
+    answer4: `English sparrow`
+    },
+    
+    //Question5
+    {
+    image: `http://dreamstop.com/wp-content/uploads/2016/08/Redwing-Blackbird.jpg`,
+    answer1: `Cardinal`,
+    answer2: `Starling`,
+    answer3: `Red-winged blackbird__correct`,
+    answer4: `Grackle`
+    },
+    
+    //Question6
+    {
+    image: `https://www.audubon.org/sites/default/files/styles/grid_gallery_lightbox/public/Pileated_Woodpecker_m57-4-021_l_1.jpg?itok=KS3GHSUB`,
+    answer1: `Downy woodpecker`,
+    answer2: `Pileated woodpecker__correct`,
+    answer3: `Red-bellied woodpecker`,
+    answer4: `Ivory billed woodpecker`
+    },
+    
+    //Question7
+    {
+    image: `https://www.allaboutbirds.org/guide/assets/photo/63667351-1900px.jpg?__hstc=46425656.2d3fcf2f99a265337744294b740e0787.1562198400035.1562198400036.1562198400037.1&__hssc=46425656.1.1562198400038&__hsfp=1817143912`,
+    answer1: `Cardinal__correct`,
+    answer2: `English sparrow`,
+    answer3: `Mockingbird`,
+    answer4: `Robin`
+    },
+    
+    //Question8
+    {
+    image: `https://www.bl.uk/britishlibrary/~/media/bl/global/language%20of%20birds/mockingbird-thinkstockphotos-533904476.jpg`,
+    answer1: `Bluejay`,
+    answer2: `Starling`,
+    answer3: `Mockingbird__correct`,
+    answer4: `Killdeer`
+    },
+    
+    //Question9
+    {
+    image: `https://www.audubon.org/sites/default/files/styles/grid_gallery_lightbox/public/Tufted_Titmouse_m17-67-821_l_1.jpg?itok=Nz-OBrmi`,
+    answer1: `English sparrow`,
+    answer2: `Starling`,
+    answer3: `Tufted titmouse__correct`,
+    answer4: `Black-capped chickadee`
+    },
+    
+    //Question10
+    {
+    image: `http://4.bp.blogspot.com/-QZhybW74CwY/Tj59_Bhn6-I/AAAAAAAAAWI/LJxv-RjkZ5o/s1600/killdeer1.JPG`,
+    answer1: `Sandpiper`,
+    answer2: `Pine thrush`,
+    answer3: `Killdeer__correct`,
+    answer4: `English sparrow`
+    }
+    ]
+    
+
 //need starting array to store quiz data
 
 //need variable to store question count and score count
 //need variable that stores submitted answer
 let questCount = 0;
 let scoreCount = 0;
+let questionArray = [];
 
 
 
@@ -16,7 +112,6 @@ let scoreCount = 0;
 function startButtonPress() {
     $(".main-container").on("click", ".start", function(event) {
         event.preventDefault();
-        window.alert('startButtonPress is working');
         startQuiz();
     });
 }
@@ -38,7 +133,7 @@ function buttonSubmitQuestion() {
 
 function buttonContinueQuiz() {
     //listens for when user presses continue button
-    continueQuiz();
+    //continueQuiz();
     //need to assess whether it is the last question of the quiz
 }
 
@@ -46,7 +141,6 @@ function buttonContinueQuiz() {
 
 function clearMainContainer() {
     $(".main-container").empty();
-    window.alert('ClearMainContainer is working');
 
 }
 
@@ -59,19 +153,78 @@ function createImageContainer(){
     //creates div class 'img-container' inside main-container
     let htmlText = `<div class="img-container"></div>`
     $(".main-container").append(htmlText);
-    window.alert("CreateImageContainer ran");
 }
 
 function createQuizContainer() {
-    //creates div class quiz-container
+    let htmlText = `<div class="quiz-content"></div>`
+    $(".main-container").append(htmlText);
 }
 
 function createQuestionResults() {
-    //creates div class question-results
+    let htmlText = `<div class="quizresults"><p>  </p></div>`
+    $(".quiz-results").append(htmlText);
 }
 
-function createQuizForm() {
-    //adds html text to quiz form to show quiz
+function createQuestionArray() {
+    let questArray = QUIZDATA[(questCount - 1)];
+    let plainTextVar = "";
+    let valueVar = "";
+    let returnArray = {};
+
+    for (let i = 1; i < 5;  i++) {
+        returnArray.length = 0;
+        let correctAns = false;
+        let iterateVar = 'answer' + String(i);
+        let answerX = questArray[iterateVar];
+
+        //need to check to see if correct answer
+            //if correct, need to splice the answer
+
+        if (String(answerX.slice(-9)) === "__correct") {
+            plainTextVar = String(answerX.slice(0,-9));
+            correctAns = true;
+        }
+        else {   
+            plainTextVar = answerX;
+            correctAns = false;
+        }
+
+        valueVar = plainTextVar.replace(" ", "_");
+
+        returnArray.plainText = plainTextVar;
+        returnArray.valueText = valueVar;
+        returnArray.booleanAnswer = correctAns;
+
+        questionArray.push(returnArray);
+    }
+
+    console.log(questionArray);
+
+}
+
+function createQuizForm(array) {
+    let addText = "";
+    let htmlText = `                
+    <form role="form">
+    <label>Which bird is this?</label><br>`;
+
+    for (let i = 0; i < 4; i++) {
+; 
+        addText = `
+        <input type="radio" name="birdType" value="` + array[i].valueText + 
+        `"><label class="birdTypeLabel">` + array[i].plainText + 
+        `</label><br>`;
+
+        htmlText = (htmlText + addText);
+
+        window.alert(addText);
+    }
+
+    $(htmlText).append(`</form>`);
+
+    $(".quiz-content").append(htmlText);
+
+    window.alert("It's working!!");   
 }
 
 function clearVariables(){
@@ -84,11 +237,11 @@ function startQuiz() {
     clearMainContainer();
     resetCountAndScore();
     createImageContainer();
-    //createQuizContainer();
-    //createQuestionResults();
+    createQuizContainer();
+    createQuestionResults();
     //createQuizForm();
     //clearVariables();
-    //nextQuestion();
+    nextQuestion();
     //nextImage();
 }
 
@@ -100,7 +253,11 @@ function clearQuizForm() {
 }
 
 function nextQuestion() {
+    questCount++
+    questionArray.length = 0;
     clearQuizForm();
+    createQuestionArray();
+    createQuizForm(questionArray);
     
     //pulls array data to next question
     //adds html code to quiz form to create answer data
@@ -181,8 +338,6 @@ function runQuizApp() {
     startButtonPress();
     restartButtonPress();
     buttonSubmitQuestion();
-
-    window.alert('runQuizApp is working');
 
     
 }
